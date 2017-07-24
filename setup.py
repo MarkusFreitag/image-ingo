7000b46acb5a68b0708edc025678a1c1f09c25b2
Markus Freitag <fmarkus@mailbox.org>
2017-07-24 19:12:10 +0200

    Introduce the setup file.

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(name='image-ingo',
+      version='0.1.0',
+      description='Python3 script for sorting images.',
+      author='Markus Freitag',
+      author_email='fmarkus@mailbox.org',
+      license='MIT',
+      packages=find_packages(),
+      install_requires=['click', 'exifread'],
+      entry_points={
+          'console_scripts': ['image-ingo=image_ingo.commands:cli'],
+          },
+      zip_safe=False)
